8ada0df6b028f3f8dc55bef34c2d2fffe4740439
Echikh Haythem <echikhhaythem09@gmail.com>
2019-02-04 10:25:42 +0100

    initial commit

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+    <p>You mail address : {{ email }}</p>
+  </div>
+</template>
+<script>
+  import axios from "axios";
+
+  export default {
+    data() {
+      return {
+        email: ""
+      };
+    },
+    created() {}
+  };
+</script>
+
+<style scoped>
+h1,
+p {
+  text-align: center;
+}
+
+p {
+  color: red;
+}
+</style>
